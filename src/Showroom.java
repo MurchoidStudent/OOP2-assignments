a18f23e6d53c4640526f7c83f2cbb58b1cabc7a4
Murchoid <kamaucoder@gmail.com>
2025-02-15 22:31:10 +0300

    CAT 1 qn 2

--- src/Showroom.java
+++ src/Showroom.java
@@ -1,3 +1,14 @@
+/*
+ * Name: Michael Kamau
+ * ADM: CT101/19151/23
+ * Course: Computer science
+ * 
+ * The answer to question 2 is in a branch called "Cat-1-qn-2" in this same repo
+ * 
+ * 
+ * 
+ */
+
 import java.util.Scanner;
 
 public class Showroom {
