568d13bd7f241db2ce014795815c15bccdbbffae
Murchoid <kamaucoder@gmail.com>
2025-02-16 17:15:16 +0300

    CAT 1 qn 1

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,3 @@
-/*
- * Name: Michael Kamau
- * ADM: CT101/19151/23
- * Course: Computer science
- * 
- * The answer to question 2 is in a branch called "Cat-1-qn-2" in this same repo
- * 
- * 
- * 
- */
-
 public class Car extends Vehicle {
 	private String fuelType;
 	
